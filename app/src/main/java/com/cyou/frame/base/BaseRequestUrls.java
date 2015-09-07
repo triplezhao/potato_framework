cae179cf1a803b8cb1bc47997098ec2649b6b4b6
zhaotianwu <zhaotianwu@3f2ce0da-b4b9-4e92-ac49-c6e80d1e7945>
2015-09-07 03:15:15 +0000

    1.目录结构修改。 将业务拿到外面。提炼出frame包

---
+++ app/src/main/java/com/cyou/frame/base/BaseRequestUrls.java
@@ -0,0 +1,13 @@
+package com.cyou.frame.base;
+
+/**
+ * 这里通常放置一些常量
+ * 
+ */
+public class BaseRequestUrls {
+
+	public static String BaseURL = "http://v.17173.com/show"; // 秀场正式地址
+	// //测试地址-新闻、视频等
+	public static String BaseJIONGURL = "http://a.17173.com"; // 正式地址-囧图
+
+}
