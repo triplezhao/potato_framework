b9ba5906354edb4b09dc2ada127de4e2edc1543b
triple.zhao@gmail.com <triple.zhao@gmail.com>
2015-09-08 15:30:50 +0800

    1master上的提交测试

--- app/src/main/java/com/cyou/demo/youku/ui/viewbinder/YKVideoViewBinder.java
+++ app/src/main/java/com/cyou/demo/youku/ui/viewbinder/YKVideoViewBinder.java
@@ -14,7 +14,7 @@ import com.cyou.frame.base.BaseViewHolder;
 import com.cyou.frame.util.ImageLoaderUtil;
 
 /**
- * Created by ztw on 2015/7/22.
+ * Created by ztw on 2015/9/8.
  */
 public class YKVideoViewBinder extends BaseViewBinder<YKVideoViewBinder.ViewHolder> {
 
