af3ba165cbba79318c622aea283cc3993e93b62b
triple.zhao@gmail.com <triple.zhao@gmail.com>
2015-11-26 10:47:41 +0800

    1.完善recyclerview 刷新 更多 控件

--- app/src/main/java/com/potato/demo/a/ui/act/BActivity.java
+++ app/src/main/java/com/potato/demo/a/ui/act/BActivity.java
@@ -5,13 +5,14 @@ import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.potato.chips.base.BaseActivity;
 import com.potato.demo.R;
 import com.potato.demo.a.data.bean.ABean;
 import com.potato.demo.a.ui.adapter.BListAdapter;
 import com.potato.demo.a.ui.fragment.Cheeses;
 import com.potato.demo.databinding.ActivityBBinding;
-import com.potato.library.view.refresh.RecyclerSwipeLayout;
+import com.potato.library.view.refresh.HFRecyclerSwipeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,13 +31,13 @@ public class BActivity extends BaseActivity {
 //        ImageView iv = (ImageView)findViewById(R.id.iv_icon);
         binding = DataBindingUtil.setContentView(
                 this, R.layout.activity_b);
-//        ImageLoaderUtil.displayImage("http://tvfan.kyodo.co.jp/wp-content/uploads/2015/01/15027b37a4104edd85fb5b79a6c9e3ac-344x516.jpg", binding.ivIcon, R.drawable.cheese_1);*/
+
+
         mAdapter = new BListAdapter(mContext);
-        binding.list.setLayoutManager(new LinearLayoutManager(mContext));
-        binding.list.setAdapter(mAdapter);
 
-
-        binding.swipeContainer.setFooterView(mContext, binding.list, R.layout.listview_footer);
+        binding.swipeContainer.setRecyclerView(binding.list,mAdapter);
+        binding.swipeContainer.setLayoutManager(new LinearLayoutManager(mContext));
+        binding.swipeContainer.setFooterView(binding.list, R.layout.listview_footer);
 
         binding.swipeContainer.setColorSchemeResources(R.color.google_blue,
                 R.color.google_green,
@@ -57,28 +58,43 @@ public class BActivity extends BaseActivity {
                 }, 300);
             }
         });
-        binding.swipeContainer.setOnLoadListener(new RecyclerSwipeLayout.OnLoadListener() {
+        binding.swipeContainer.setOnLoadListener(new HFRecyclerSwipeLayout.OnLoadListener() {
             @Override
             public void onLoad() {
                 mHandler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         mValues.addAll(getRandomSublist(Cheeses.sCheeseStrings, 30));
+                        int lastCount = mValues.size();
                         mAdapter.setDataList(mValues);
 //                        mAdapter.notifyDataSetChanged();
+                        mAdapter.notifyItemInserted(lastCount);
                         binding.swipeContainer.setLoading(false);
                     }
                 }, 300);
 
             }
         });
+
+        binding.swipeContainer.setScrollStateLisener(new HFRecyclerSwipeLayout.ScrollStateLisener() {
+            @Override
+            public void pause() {
+                ImageLoader.getInstance().pause();
+
+            }
+
+            @Override
+            public void resume() {
+                ImageLoader.getInstance().resume();
+            }
+        });
+
         binding.swipeContainer.setRefreshing(true);
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 mValues = getRandomSublist(Cheeses.sCheeseStrings, 30);
                 mAdapter.setDataList(mValues);
-                mAdapter.notifyDataSetChanged();
                 binding.swipeContainer.setRefreshing(false);
             }
         }, 300);
---
+++ library/src/main/java/com/potato/library/view/hfrecyclerview/HFGridlayoutSpanSizeLookup.java
@@ -0,0 +1,25 @@
+package com.potato.library.view.hfrecyclerview;
+
+import android.support.v7.widget.GridLayoutManager;
+
+/**
+ * Created by cundong on 2015/10/23.
+ * <p/>
+ * RecyclerView为GridLayoutManager时，设置了HeaderView，就会用到这个SpanSizeLookup
+ */
+public class HFGridlayoutSpanSizeLookup extends GridLayoutManager.SpanSizeLookup {
+
+    private HFRecyclerViewAdapter adapter;
+    private int mSpanSize = 1;
+
+    public HFGridlayoutSpanSizeLookup(HFRecyclerViewAdapter adapter, int spanSize) {
+        this.adapter = adapter;
+        this.mSpanSize = spanSize;
+    }
+
+    @Override
+    public int getSpanSize(int position) {
+        boolean isHeaderOrFooter = adapter.isHeader(position) || adapter.isFooter(position);
+        return isHeaderOrFooter ? mSpanSize : 1;
+    }
+}
