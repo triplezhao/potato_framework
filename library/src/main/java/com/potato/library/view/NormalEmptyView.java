ea3de8860126ca2c5543e63320d25d0f3583ae7e
triple.zhao@gmail.com <triple.zhao@gmail.com>
2017-05-20 19:03:05 +0800

    1.normalEmptyView的 init方法优化，as中可以预览了

--- library/src/main/java/com/potato/library/view/NormalEmptyView.java
+++ library/src/main/java/com/potato/library/view/NormalEmptyView.java
@@ -8,25 +8,26 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
 import com.potato.library.R;
 import com.potato.library.util.L;
 
 
-public class NormalEmptyView extends RelativeLayout implements EmptyViewInterface{
-    public static final int EMPTY_TYPE_LOADING = 1;
-    public static final int EMPTY_TYPE_ERROR = 2;
+public class NormalEmptyView extends RelativeLayout implements EmptyViewInterface {
+    public static final int EMPTY_TYPE_LOADING   = 1;
+    public static final int EMPTY_TYPE_ERROR     = 2;
     public static final int EMPTY_TYPE_NOCONTENT = 3;
-    public static final int EMPTY_TYPE_GONE = 4;
-    public static final int EMPTY_SHOW = 5;
+    public static final int EMPTY_TYPE_GONE      = 4;
+    public static final int EMPTY_SHOW           = 5;
 
     public LinearLayout ll_empty;
-    public TextView tv_text;
-    public View pb_loading;
-    public ImageView iv_pic;
+    public TextView     tv_text;
+    public View         pb_loading;
+    public ImageView    iv_pic;
 
-    public String mEmptyTxt = "内容暂时为空，敬请等待~";
+    public String mEmptyTxt   = "内容暂时为空，敬请等待~";
     public String mLoadingTxt = "内容获取中...";
-    public String mErrorTxt = "数据加载异常，请稍后重试";
+    public String mErrorTxt   = "数据加载异常，请稍后重试";
 
     public int mEmptyDrawableRes = R.drawable.empty_nocontent;
     public int mErrorDrawableRes = R.drawable.empty_nocontent;
@@ -34,7 +35,7 @@ public class NormalEmptyView extends RelativeLayout implements EmptyViewInterfac
     public int mEmptyType = EMPTY_TYPE_GONE;
 
     public String mTxt = mEmptyTxt;
-    public int mPic = mEmptyDrawableRes;
+    public int    mPic = mEmptyDrawableRes;
 
     public Context mContext;
 
@@ -45,13 +46,15 @@ public class NormalEmptyView extends RelativeLayout implements EmptyViewInterfac
     public NormalEmptyView(Context context, AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
-        LayoutInflater.from(context).inflate(R.layout.potato_normal_empty, this);
+        LayoutInflater
+                .from(context)
+                .inflate(R.layout.potato_normal_empty, this);
         ll_empty = (LinearLayout) findViewById(R.id.ll_empty);
         tv_text = (TextView) findViewById(R.id.tv_text);
         pb_loading = (View) findViewById(R.id.pb_loading);
         iv_pic = (ImageView) findViewById(R.id.iv_pic);
         iv_pic.setImageResource(mEmptyDrawableRes);
-        L.i("NormalEmptyView", "NormalEmptyView()");
+//        L.i("NormalEmptyView", "NormalEmptyView()");
     }
 
     public void showError() {
